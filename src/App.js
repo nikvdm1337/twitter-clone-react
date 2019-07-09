46e7f7d2f7b4e4fd43f448d66609da6d22a6596f
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-09 16:33:20 +0700

    mounting components to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Sidebar from './Sidebar'
+import Content from './Content'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	// Data
+	state = {
+		channel: ''
+	}
+	// Functions
+	getMessages = (id) => {
+		this.setState({
+			channel: id
+		})
+	}
+	// Render
+	render() {
+		return (
+			<div id="wrap">
+				<Sidebar getMessages={this.getMessages} />
+				<Content channel={this.state.channel} />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
