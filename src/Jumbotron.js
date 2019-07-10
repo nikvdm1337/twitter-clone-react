0e9024aa9ba454f3423182e58345e82842000442
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-11 01:29:31 +0700

    added jumbotron component with bg-img + styling

---
+++ src/Jumbotron.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Jumbotron extends Component {
+
+    render () {
+        return (
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Tortuga Twitter</h1>
+          <p className="lead">9-week all-inclusive web development bootcamp in Thailand. Learn to Code in Paradise.</p>
+        </div>
+      </div>
+    )}
+}
+
+export default Jumbotron
